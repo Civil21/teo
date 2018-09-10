109e99084f6e76ead1684662921790edeaccfee3
Civil21 <diusolexander@gmail.com>
2018-09-10 18:21:26 +0300

    add messagen action

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+//основні компоненти що поставляються діскордом
 const Discord = require("discord.js");
 const teo = new Discord.Client();
+//секретний ключ
 const token = require("./token.json");
 
+//реакція на запуск 
 teo.on("ready", async()=>{
-console.log("Вітаю майстер!");
-//console.log(teo);
-console.log(`Я ${teo.user.username} слідкую за ${teo.guilds.size} гільдіями `);
+    console.log("Вітаю майстер!");
+    //console.log(teo);
+    console.log(`Я ${teo.user.username} слідкую за ${teo.guilds.size} гільдіями `);
 });
+
+//реакція на повідомлення 
+teo.on("message", message => {
+    //якщо повідомлення від бота подальша взаємодія непотрібна
+    if (message.author.bot) return ;
+
+    //визначаємо з ким і де будемо спілкуватись (з користувачем на каналі чи в приватних повідомленнях)
+    let targetSend = message.guild?message.channel:message.author;
+
+    targetSend.send("Вітаю! Я Тео.");
+
+}); 
 
 teo.login(token)
