d493c787d6d047477b17d0e1a7b5c798f46bfb5c
Civil21 <diusolexander@gmail.com>
2018-09-10 18:44:45 +0300

    lastInfo create

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //основні компоненти що поставляються діскордом
 const Discord = require("discord.js");
 const teo = new Discord.Client();
+
+const fs = require("fs");
 //секретний ключ
 const token = require("./token.json");
+//зберігає останні данні про користувача і його повідомлення
+let lastInfo = require("./lastInfo.json");
 
 //реакція на запуск 
 teo.on("ready", async()=>{
@@ -16,10 +20,22 @@ teo.on("message", message => {
     //якщо повідомлення від бота подальша взаємодія непотрібна
     if (message.author.bot) return ;
 
-    //визначаємо з ким і де будемо спілкуватись (з користувачем на каналі чи в приватних повідомленнях)
-    let targetSend = message.guild?message.channel:message.author;
+    //визначаємо з ким будемо спілкуватись
+    let user = message.author;
+    //визначаємо де будемо спілкуватись (на каналі чи в приватних повідомленнях)
+    let targetSend = message.guild?message.channel:user;
 
-    targetSend.send("Вітаю! Я Тео.");
+    if(lastInfo[user.id]){
+        targetSend.send(`Минулий раз користувач з id ${user.id}(${user.username}) сказав ${lastInfo[user.id].message}`);
+    }
+    lastInfo[user.id]={"message" : message.content};
+    fs.writeFile("./lastInfo.json", JSON.stringify(lastInfo), (err) => {
+        if (err) console.error(err)
+    });
+    console.log(lastInfo);
+    targetSend.send(`Користувач з id ${user.id}(${user.username}) сказав ${message.content}`);
+    
+    //targetSend.send("Вітаю! Я Тео.");
 
 }); 
 
