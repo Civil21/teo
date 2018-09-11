ce21c1eaeb940a2269069d4a583e5b21f1bd8d1e
Civil21 <diusolexander@gmail.com>
2018-09-11 18:12:05 +0300

    small update

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const token = require("./token.json");
 //зберігає останні данні про користувача і його повідомлення
 let lastInfo = require("./lastInfo.json");
 //зберіграє нібр реплік для спілкування та взаємодії
-let dictionary = require(".ua/dictionary.json"); 
+let local = "./ru/dictionary.json";
+let dictionary = require(local); 
 
 //реакція на запуск 
 teo.on("ready", async()=>{
@@ -21,50 +22,51 @@ teo.on("ready", async()=>{
 teo.on("message", message => {
     //якщо повідомлення від бота подальша взаємодія непотрібна
     if (message.author.bot) return ;
-
     //визначаємо з ким будемо спілкуватись
     let user = message.author;
     //визначаємо де будемо спілкуватись (на каналі чи в приватних повідомленнях)
     let targetSend = message.guild?message.channel:user;
-
-
+    //обробка тексту для аналізу та зберігання 
+    let content=message.content.toLowerCase();
+    
     console.log(`test 1 ${lastInfo[user.id]!=null}`);
     console.log(`test 2 ${message.createdTimestamp-lastInfo[user.id].time<=60000}`);
     console.log(`test 3 ${lastInfo[user.id].answer != ""}`);
-    if(lastInfo[user.id] 
-        && lastInfo[user.id].answer != "" 
-        && message.createdTimestamp-lastInfo[user.id].time<=2400000){
+    
+    
+    if(lastInfo[user.id])
+    if(lastInfo[user.id].answer != "" && message.createdTimestamp-lastInfo[user.id].time<=2400000){
         //targetSend.send(`Минулий раз користувач з id ${user.id}(${user.username}) сказав ${lastInfo[user.id].message}`);
 
-        if(dictionary[lastInfo[user.id].answer] && dictionary[lastInfo[user.id].answer].indexOf(message.content.toLowerCase())<0){
+        if(dictionary[lastInfo[user.id].answer] && dictionary[lastInfo[user.id].answer].indexOf(content)<0){
             answers = dictionary[lastInfo[user.id].answer];
-            console.log(answers);
+            //console.log(answers);
         }else{
             answers = []
-            console.log("NEW");
+            //console.log("NEW");
         }
-        answers.push(message.content.toLowerCase());
+        answers.push(content);
         dictionary[lastInfo[user.id].answer]= answers;
-        fs.writeFile("./dictionary.json", JSON.stringify(dictionary), (err) => {
+        fs.writeFile(local, JSON.stringify(dictionary), (err) => {
             if (err) console.error(err)
         });
     }
     let answer 
-    console.log(dictionary[message.content.toLowerCase()]);
-    if(dictionary[message.content.toLowerCase()]){
-        console.log(dictionary);
-        answers = dictionary[message.content.toLowerCase()];
+    //console.log(dictionary[content]);
+    if(dictionary[content]){
+        //console.log(dictionary);
+        answers = dictionary[content];
         let r=Math.floor((Math.random(1) * answers.length));
         answer = answers[r];
-        targetSend.send(answer);
+        targetSend.send(message.author.username+" "+answer);
     }
 
-    lastInfo[user.id]={"message" : message.content, "time" : message.createdTimestamp, "answer": answer };
+    lastInfo[user.id]={"time" : message.createdTimestamp, "answer": answer };
     fs.writeFile("./lastInfo.json", JSON.stringify(lastInfo), (err) => {
         if (err) console.error(err)
     });
-    console.log(lastInfo);
-    //targetSend.send(`Користувач з id ${user.id}(${user.username}) сказав ${message.content}`);
+    //console.log(lastInfo);
+    //targetSend.send(`Користувач з id ${user.id}(${user.username}) сказав ${content}`);
     
     //targetSend.send("Вітаю! Я Тео.");
 
