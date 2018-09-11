9001f9dc7fbbff569649cf5798177ca4dff8a7a4
Civil21 <diusolexander@gmail.com>
2018-09-11 19:16:12 +0300

    teach mod

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const teo = new Discord.Client();
 const fs = require("fs");
 //секретний ключ
 const token = require("./token.json");
+//управління режимом навчання
+let teach = false;
 //зберігає останні данні про користувача і його повідомлення
 let lastInfo = require("./lastInfo.json");
 //зберіграє нібр реплік для спілкування та взаємодії
@@ -16,25 +18,55 @@ teo.on("ready", async()=>{
     console.log("Вітаю майстер!");
     //console.log(teo);
     console.log(`Я ${teo.user.username} слідкую за ${teo.guilds.size} гільдіями `);
+    console.log(`У мене ${teo.users.size} друзів`);
 });
 
 //реакція на повідомлення 
 teo.on("message", message => {
+    //console.log("New message");
     //якщо повідомлення від бота подальша взаємодія непотрібна
     if (message.author.bot) return ;
+
     //визначаємо з ким будемо спілкуватись
     let user = message.author;
     //визначаємо де будемо спілкуватись (на каналі чи в приватних повідомленнях)
     let targetSend = message.guild?message.channel:user;
+
+    if (message.content.startsWith("Тео")){
+        let args = message.content.toLowerCase().split(" ");
+        switch(args[1]){
+            case "повинен":
+                switch(args[2]){
+                    case "вчитись":
+                        teach = true;
+                        targetSend.send("Тео буде!!");
+                    break;
+                }
+            break;
+            case "не":
+                switch(args[2]){
+                    case "повинен" :
+                        switch(args[3]){
+                            case "вчитись":
+                                teach = false;
+                                targetSend.send("Тео не буде!!");
+                            break;
+                        }
+                    break;
+                }
+            break;
+        }
+        return;
+    }
     //обробка тексту для аналізу та зберігання 
     let content=message.content.toLowerCase();
-    
+    /*
     console.log(`test 1 ${lastInfo[user.id]!=null}`);
     console.log(`test 2 ${message.createdTimestamp-lastInfo[user.id].time<=60000}`);
     console.log(`test 3 ${lastInfo[user.id].answer != ""}`);
+    */
     
-    
-    if(lastInfo[user.id])
+    if(lastInfo[user.id]&&teach)
     if(lastInfo[user.id].answer != "" && message.createdTimestamp-lastInfo[user.id].time<=2400000){
         //targetSend.send(`Минулий раз користувач з id ${user.id}(${user.username}) сказав ${lastInfo[user.id].message}`);
 
@@ -65,11 +97,11 @@ teo.on("message", message => {
     fs.writeFile("./lastInfo.json", JSON.stringify(lastInfo), (err) => {
         if (err) console.error(err)
     });
+
     //console.log(lastInfo);
     //targetSend.send(`Користувач з id ${user.id}(${user.username}) сказав ${content}`);
-    
-    //targetSend.send("Вітаю! Я Тео.");
 
+    //targetSend.send("Вітаю! Я Тео.");
 }); 
 
 teo.login(token)
